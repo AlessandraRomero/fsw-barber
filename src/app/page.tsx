f87046804a80e5bf93874e26fcf2edd0eef94c9e
AlessandraRomero <alessandraromeiro098@gmil.com>
2024-08-07 17:31:03 -0400

    feat: add BookingItem component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,82 @@
 import { SearchIcon } from "lucide-react"
 import Image from "next/image"
+import BarbershopItem from "./_components/barbershop-item"
+import BookingItem from "./_components/booking-item"
 import Header from "./_components/header"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
-import { Badge } from "./_components/ui/badge"
 import { Button } from "./_components/ui/button"
 import { Card, CardContent } from "./_components/ui/card"
 import { Input } from "./_components/ui/input"
+import { QuickSearchOptions } from "./_constants/search"
+import { db } from "./_lib/prisma"
 
-const Home = () => (
-  <div>
-    <Header />
-    <div className="p-5">
-      <h2>Olá, Alessandra!</h2>
-      <p>Quarta-feira, 7 de agosto</p>
-      <div className="mt-6 flex items-center gap-2">
-        <Input placeholder="Faça sua busca..." />
-        <Button>
-          <SearchIcon />
-        </Button>
+const Home = async () => {
+  const barbershops = await db.barbershop.findMany({})
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  })
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2>Olá, Alessandra!</h2>
+        <p>Quarta-feira, 7 de agosto</p>
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          {QuickSearchOptions.map((option) => (
+            <Button className="gap-2" variant="secondary" key={option.title}>
+              <Image
+                alt={option.title}
+                src={option.imageUrl}
+                width={16}
+                height={16}
+              />
+              {option.title}
+            </Button>
+          ))}
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+        <BookingItem />
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
-      <div className="relative mt-6 h-[150px] w-full">
-        <Image
-          alt="Agende nos melhores com FSW Barber"
-          src="/banner-01.png"
-          fill
-          className="rounded-xl object-cover"
-        />
-      </div>
-      <Card className="mt-6">
-        <CardContent className="flex justify-between p-0">
-          <div className="flex flex-col gap-2 py-5 pl-5">
-            <Badge className="w-fit">Confirmado</Badge>
-            <h3 className="font-semibold">Corte de Cabelo</h3>
-            <div className="flex items-center gap-2">
-              <Avatar className="h-6 w-6">
-                <AvatarImage src="https://utfs.io/f/5832df58-cfd7-4b3f-b102-42b7e150ced2-16r.png" />
-              </Avatar>
-              <p className="text-sm">Barbearia FSW</p>
-            </div>
-          </div>
-          <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-            <p className="text-sm">Agosto</p>
-            <p className="text-2xl">07</p>
-            <p className="text-sm">13:07</p>
-          </div>
-        </CardContent>
-      </Card>
+      <footer>
+        <Card>
+          <CardContent className="px-5 py-6">
+            <p className="text-sm text-gray-400">
+              © 2024 Copyright <span className="font-bold">FSW Barber</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
-  </div>
-)
-
+  )
+}
 export default Home
